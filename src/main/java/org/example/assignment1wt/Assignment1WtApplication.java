1a0257b4874b5e31a7bd872a9370d557850628de
TolbasyA <254617@astanait.edu.kz>
2026-09-08 15:33:37 +0500

    asik1

---
+++ src/main/java/org/example/assignment1wt/Assignment1WtApplication.java
@@ -0,0 +1,13 @@
+package org.example.assignment1wt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1WtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment1WtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/assignment1wt/Assignment1WtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.assignment1wt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1WtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
